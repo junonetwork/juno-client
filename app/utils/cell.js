1fb9bb4cc5bb1f77bf92b5180486c4679741b2ab
James Conkling <james.lane.conkling@gmail.com>
2018-01-17 22:39:58 -0500

    add edit empty cell epic

--- app/utils/cell.js
+++ app/utils/cell.js
@@ -10,6 +10,10 @@ export const destructureAddress = (address) => {
   return { sheetId, column: +column, row: +row, };
 };
 
+export const getUpCell = (matrix, column, row) => matrix[row - 1] && matrix[row - 1][column];
+
+export const getLeftCell = (matrix, column, row) => matrix[row] && matrix[row][column - 1];
+
 
 /**
  * @param {String} sheetId
