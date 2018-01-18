2b098090c96ed58529eae3a67b8a3e4866d53ab3
James Conkling <james.lane.conkling@gmail.com>
2018-01-17 20:32:15 -0500

    add default error handling to actionStream

--- app/redux/actionStream.js
+++ app/redux/actionStream.js
@@ -4,30 +4,42 @@ import {
 import { from }                   from 'rxjs/observable/from';
 import { merge }                  from 'rxjs/observable/merge';
 import { batchActions }           from 'redux-batched-actions';
+import {
+  compose,
+  equals,
+  prop,
+  identity,
+}                                 from 'ramda';
+import {
+  catchError,
+}                                 from 'rxjs/operators';
 
 
 export default (store, epics) => {
-  const { handler, stream } = createEventHandlerWithConfig({
+  const { handler, stream, } = createEventHandlerWithConfig({
     fromESObservable: from,
-    toESObservable: stream => stream,
+    toESObservable: identity,
   })();
 
   merge(
     ...epics.map((epic) => stream.pipe(epic(store)))
   )
+    .pipe(catchError((error, caught) => {
+      console.error('Uncaught error on the action stream:', error);
+      return caught;
+    }))
     .subscribe({
       next: (action) => {
         if (Array.isArray(action)) {
-          store.dispatch(batchActions(action)); 
-        } else {
-          store.dispatch(action); 
+          store.dispatch(batchActions(action));
+        } else if (action !== null && action !== undefined) {
+          store.dispatch(action);
         }
       },
-      error: (error) => {
-        console.error('Redux Event Stream Error', error);
-      }
     });
 
   return handler;
 };
 
+
+export const ofType = (type) => compose(equals(type), prop('type'));
