49db33dae12da7421f250bebf32f950cacf4dde1
James Conkling <james.lane.conkling@gmail.com>
2018-03-11 22:56:25 -0400

    relax lint rules around a11ly

--- app/components/IndexInput/indexInput.jsx
+++ app/components/IndexInput/indexInput.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React                 from 'react';
 import {}                    from 'prop-types';
 import                            './style.scss';
--- app/components/Input/Input.jsx
+++ app/components/Input/Input.jsx
@@ -1,9 +1,7 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React                   from 'react';
 import {
   string,
   shape,
-  func
 }                              from 'prop-types';
 import                              './style.scss';
 
